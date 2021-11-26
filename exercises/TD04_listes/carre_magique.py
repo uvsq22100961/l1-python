46f14a1bbeb94183c33f50b4ecb7a8859a5043f6
Thibault ROYERE <thibault.royere@ens.uvsq.fr>
2021-11-26 10:50:49 +0100

    carre_magique_v1

---
+++ exercises/TD04_listes/carre_magique.py
@@ -0,0 +1,12 @@
+#3. Carré magique
+
+#1. Sans écrire de programme, vérifier qu'il s'agit bien d'un carré magique, et donner sa constante magique.
+
+34
+
+#Les questions qui suivent doivent être traitées dans un script, qui peut s'appeler `carre_magique.py`. Pensez à utiliser
+#  [PythonTutor](http://pythontutor.com/) et les outils de programmation pour résoudre les problèmes qui pourraient survenir.
+
+#2. Créer une liste à deux dimensions affectée à  la variable `carre_mag` contenant ce carré magique.
+
+carre_mag = [4, 14, 15, 1]
